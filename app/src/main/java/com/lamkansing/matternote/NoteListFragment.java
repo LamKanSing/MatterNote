54f1570927749a829a8be436ddc5098c11fa0270
lamkansing <lam.kan.sing@gmail.com>
2014-12-08 17:06:46 +0800

    add cursoradapter to listview

--- app/src/main/java/com/lamkansing/matternote/NoteListFragment.java
+++ app/src/main/java/com/lamkansing/matternote/NoteListFragment.java
@@ -14,30 +14,23 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class NoteListFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_NOTEBOOKTITLE = "notebooktitle";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String notebookTitle;
 
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param notebookTitle Parameter 1.
      * @return A new instance of fragment NoteListFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static NoteListFragment newInstance(String param1, String param2) {
+    public static NoteListFragment newInstance(String notebookTitle) {
         NoteListFragment fragment = new NoteListFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_NOTEBOOKTITLE, notebookTitle);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,8 +43,7 @@ public class NoteListFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            notebookTitle = getArguments().getString(ARG_NOTEBOOKTITLE);
         }
     }
 
@@ -62,5 +54,8 @@ public class NoteListFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_note_list, container, false);
     }
 
-
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
 }
