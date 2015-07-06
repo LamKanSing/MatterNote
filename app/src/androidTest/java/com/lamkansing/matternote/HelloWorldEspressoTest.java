63fd00ec9976a96887da62ab69296a70edf985c1
lamkansing <lam.kan.sing@gmail.com>
2015-07-06 20:14:05 +0800

    check fab add notebook

--- app/src/androidTest/java/com/lamkansing/matternote/HelloWorldEspressoTest.java
+++ app/src/androidTest/java/com/lamkansing/matternote/HelloWorldEspressoTest.java
@@ -4,6 +4,7 @@ import android.test.ActivityInstrumentationTestCase2;
 import static android.support.test.espresso.Espresso.*;
 import static android.support.test.espresso.matcher.ViewMatchers.*;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import android.support.test.espresso.action.ViewActions;
 
 /**
  * Created by line on 7/5/15.
@@ -20,7 +21,19 @@ public class HelloWorldEspressoTest extends ActivityInstrumentationTestCase2<Not
         getActivity();
     }
 
-    public void testListGoesOverTheFold() {
-        onView(withText("Hello world!")).check(matches(isDisplayed()));
+    public void testCreateNewNotebook(){
+        onView(withId(R.id.fabaddnotebook)).check(matches(isDisplayed()));
+        onView(withId(R.id.fabaddnotebook)).perform(ViewActions.click());
+
+        // on dialog fragment, enter new notebook name
+        onView(withId(R.id.editText_notebookname)).check(matches(isDisplayed()));
+        onView(withId(R.id.editText_notebookname)).perform(ViewActions.typeText("testing"));
+        onView(withText("save")).perform(ViewActions.click());
     }
+
+    /*
+    public void testClickListNotebook(){
+        onData(allOf(is(instanceOf(String.class)), is("Americano")))
+                .perform(click());
+    }*/
 }
